5083821b322190a387b19fbc579e044c3feaeea6
yangkaipeng <yangkaipeng2012@163.com>
2020-11-15 18:09:03 +0800

    .gitignore

--- demo0620/users/urls.py
+++ demo0620/users/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('register/', views.regitser, name='register'),
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('profile/<int:pk>/', views.profile, name='profile'),
     path('profile/<int:pk>/update/', views.profile_update, name='profile_update'),
